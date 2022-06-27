b4cc65668d35b29a8da05a61266a979cb98029f1
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-06-27 18:59:30 +0530

    code: created playground for swift practice

---
+++ Code/Playgrounds/Fundamentals.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+var mood = "Happy"
+// variables can be reassigned
+mood = "excited"
+
+// constants cannot be reassigned
+let pain = "constant"
+
+// Multi-line strings
+let fact = """
+Exercising is
+good for
+YOU!
+"""
+
