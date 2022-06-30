2a38fb7f83a3b9c998d159bafe02aeaef56ed545
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-06-30 20:54:50 +0530

    code: practice code for checkpoint 2

--- Code/Playgrounds/Fundamentals.playground/Contents.swift
+++ Code/Playgrounds/Fundamentals.playground/Contents.swift
@@ -1,6 +1,12 @@
 import Cocoa
 
-// adding the decimal point makes it a Double
-let celsius = 38.0
-let fahrenheit = (celsius * 9.0) / 5.0 + 32.0
-print("\(fahrenheit)°")
+// 1. Create a string of arrays
+let carCompanies = ["Porsche", "Ferrari", "BMW", "Mercedes", "Aston Martin", "bmw", "Porsche"]
+
+// 2. print number of elements in the array
+print(carCompanies.count)
+
+// 3. print the number of unique elements in the array
+// here Porsche and BMW are repeated. But as BMW has different cases, it will count as unique.
+let uniqueCompaniesArray = Set(carCompanies)
+print(uniqueCompaniesArray.count)
