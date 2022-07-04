60404cff4b32804868afa6aa4fc02e18ae6468a6
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-07-04 19:30:11 +0530

    code: solution for checkpoint 4

--- Code/Playgrounds/Fundamentals.playground/Contents.swift
+++ Code/Playgrounds/Fundamentals.playground/Contents.swift
@@ -1,19 +1,39 @@
 import Cocoa
 
-// FizzBuzz
-// loop over range of 1...100
-// if number is multiple of 3, print Fizz
-// if number is multiple of 5, print Buzz
-// if number is multiple of 3 & 5, print FizzBuzz
+// define enum that has our errors
+enum SqrtError: Error {
+  case outOfBound
+  case noRoot
+}
 
-for i in 1...100 {
-  if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
-    print("FizzBuzz")
-  } else if i.isMultiple(of: 3) {
-    print("Fizz")
-  } else if i.isMultiple(of: 5){
-    print("Buzz")
-  } else {
-    print(i)
+func getRoot(for number: Int) throws -> Int {
+  if number < 1 || number > 10_000 { throw SqrtError.outOfBound }
+  
+  var start = 1
+  var end = number
+  while start <= end {
+    let mid = start + (end - start) / 2
+    
+    if mid * mid == number {
+      return mid
+    } else if mid * mid < number {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
   }
+  
+  throw SqrtError.noRoot
+}
+
+do {
+  let num = 100_000
+  let root = try getRoot(for: num)
+  print(root)
+} catch SqrtError.noRoot {
+  print("Given integer has no square root")
+} catch SqrtError.outOfBound {
+  print("Out of bounds")
+} catch {
+  print("Some error occured")
 }
