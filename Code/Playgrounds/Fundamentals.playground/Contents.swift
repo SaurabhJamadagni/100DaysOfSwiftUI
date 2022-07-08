0c193bc2789f69c8c794666c8cd669901435e167
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-07-08 21:58:47 +0530

    code: solution for checkpoint 7

--- Code/Playgrounds/Fundamentals.playground/Contents.swift
+++ Code/Playgrounds/Fundamentals.playground/Contents.swift
@@ -1,11 +1,67 @@
 import Cocoa
 
-// filter, sort, map without temporary variables
-let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
-
-var finalArray = luckyNumbers.filter { !$0.isMultiple(of: 2) }.sorted { $0 < $1 }.map { String($0) }
-
-for num in finalArray {
-  print("\(num) is a lucky number")
+class Animal {
+  let legs: Int
+  
+  init(legs: Int) {
+    self.legs = legs
+  }
 }
 
+class Dog: Animal {
+  func speak() {
+    print("I am a dog, woof woof!")
+  }
+}
+
+class Corgi: Dog {
+  override func speak() {
+    print("I am a corgi, woof woof!")
+  }
+}
+
+class Poodle: Dog {
+  override func speak() {
+    print("I am a poodle, woof woof!")
+  }
+}
+
+class Cat: Animal {
+  var isTame: Bool
+  
+  init(isTame: Bool, legs: Int) {
+    self.isTame = isTame
+    super.init(legs: legs)
+  }
+  
+  func speak() {
+    print("I am a cat")
+  }
+}
+
+class Persian: Cat {
+  override func speak() {
+    print("I am a cat and I purr")
+  }
+}
+
+class Lion: Cat {
+  override func speak() {
+    print("I am a cat and I roar")
+  }
+}
+
+let dog = Dog(legs: 4)
+dog.speak()
+let corgi = Corgi(legs: 4)
+corgi.speak()
+let poodle = Poodle(legs: 4)
+poodle.speak()
+
+let cat = Cat(isTame: true, legs: 4)
+cat.speak()
+let persian = Persian(isTame: true, legs: 4)
+persian.speak()
+let lion = Lion(isTame: false, legs: 4)
+lion.speak()
+print("Is a lion tameable? \(lion.isTame)")
