acd02a4cc0b55e5696ad35229dcd93ed771d431a
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-07-11 23:54:48 +0530

    code: initial commit for WeSplit project

---
+++ Code/WeSplit-P1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Saurabh on 11/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
