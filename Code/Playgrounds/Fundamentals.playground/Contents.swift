56be158a557d7f8bd2698b4bdecf0e5c95158ff8
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-07-11 09:56:26 +0530

    code: solution for checkpoint 9

--- Code/Playgrounds/Fundamentals.playground/Contents.swift
+++ Code/Playgrounds/Fundamentals.playground/Contents.swift
@@ -1,45 +1,8 @@
 import Cocoa
 
-protocol Building {
-  var numOfRooms: Int { get }
-  var cost: Int { get }
-  var agentName: String { get set }
-  
-  func printSalesSummary()
+func getRandomElement(numbers: [Int]?) -> Int {
+  numbers?.randomElement() ?? Int.random(in: 1...100)
 }
 
-struct House: Building {
-  var numOfRooms: Int
-  var cost: Int
-  var agentName: String
-  
-  func printSalesSummary() {
-    print("This house has \(numOfRooms) rooms")
-    print("It costs $\(cost)")
-    print("The current RealEstate agent is \(agentName)")
-  }
-}
-
-struct Office: Building {
-  var numOfRooms: Int
-  var cost: Int
-  var agentName: String
-  var isOnLease: Bool
-  
-  func printSalesSummary() {
-    print("This office has \(numOfRooms) rooms")
-    print("It costs $\(cost)")
-    print("The current RealEstate agent is \(agentName)")
-    if !isOnLease {
-      print("currently available for lease")
-    }
-  }
-}
-
-var house = House(numOfRooms: 4, cost: 400_000, agentName: "Scrooge McDuck")
-house.printSalesSummary()
-
-var office = Office(numOfRooms: 30, cost: 12_000_000, agentName: "Money Forall", isOnLease: false)
-office.printSalesSummary()
-office.numOfRooms = 50
-office.printSalesSummary()
+print(getRandomElement(numbers: [5, 7, 1, 2]))
+print(getRandomElement(numbers: nil))
